772db47c0959a4ab1d78dffade56da91698543eb
LuanJLima <luan.jonatas@hotmail.com>
2024-08-29 11:38:44 -0300

    modelo

--- ex004/index.php
+++ ex004/index.php
@@ -4,20 +4,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Apresente-se para nós</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <h1>
+    <header>
+        <h1>Apresente-se para nós</h1>
+    </header>
 
-    </h1>
-
-    <?php 
-        // operador de concatenaçao é o .
-        // EX: "Curso" . "PHP" = "Curso PHP"
-
-        
-    ?>
+    <section>
+        <form action="cad.php" method="get">
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" id="idnome">
+            <label for="sobrenome">Sobrenome</label>
+            <input type="text" name="sobrenome" id="idsobrenome">
+            <input type="submit" value="Eviar">
+        </form>
+    </section>
 </body>
 
 </html>
